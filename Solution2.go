2d1150703187f9f878172010d2e817d0a6be6cea
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-04-09 19:34:01 +0800

    二叉搜索树+合并二叉树

--- Solution2.go
+++ Solution2.go
@@ -27,3 +27,39 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 		Right: constructMaximumBinaryTree(nums[index+1:]),
 	}
 }
+
+// 合并二叉树
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	var dfs func(node1 *TreeNode, node2 *TreeNode) *TreeNode
+	dfs = func(node1 *TreeNode, node2 *TreeNode) *TreeNode {
+		if node1 == nil || node2 == nil {
+			if node1 == nil {
+				return node2
+			}
+			return node1
+		}
+		node1.Val += node2.Val
+		node1.Left = dfs(node1.Left, node2.Left)
+		node1.Right = dfs(node1.Right, node2.Right)
+		return node1
+	}
+	return dfs(root1, root2)
+}
+
+// 二叉搜索树中的搜索
+func searchBST(root *TreeNode, val int) *TreeNode {
+	var dfs func(node *TreeNode) *TreeNode
+	dfs = func(node *TreeNode) *TreeNode {
+		if node == nil {
+			return nil
+		}
+		if node.Val < val {
+			return dfs(node.Right)
+		} else if node.Val > val {
+			return dfs(node.Left)
+		} else {
+			return node
+		}
+	}
+	return dfs(root)
+}

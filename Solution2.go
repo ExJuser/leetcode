ac2f4e83015bf1b50ca9c1ce73d1e8ba7f58edfb
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-04-11 19:03:00 +0800

    有序数组转换为二叉搜索树

--- Solution2.go
+++ Solution2.go
@@ -299,3 +299,16 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
 		return root
 	}
 }
+
+// 将有序数组转换为二叉搜索树
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+}

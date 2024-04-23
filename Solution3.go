2b8bfad01a07b1e54c9a0835294b28c620cc2192
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-04-23 18:49:46 +0800

    最大子数组和

--- Solution3.go
+++ Solution3.go
@@ -142,3 +142,13 @@ func wiggleMaxLength(nums []int) int {
 	}
 	return max(slices.Max(dp1), slices.Max(dp2))
 }
+
+// 最大子数组和的最优解
+func maxSubArray(nums []int) int {
+	ans := nums[0]
+	for i := 1; i < len(nums); i++ {
+		nums[i] = max(nums[i], nums[i-1]+nums[i])
+		ans = max(ans, nums[i])
+	}
+	return ans
+}

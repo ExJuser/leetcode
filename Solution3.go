d25276d2f1a5e47d85e46e9e83998331f366c31a
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-09 19:50:40 +0800

    不同路径1、2+最小花费爬楼梯

--- Solution3.go
+++ Solution3.go
@@ -478,3 +478,60 @@ func monotoneIncreasingDigits(n int) (ans int) {
 	}
 	return
 }
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost)+1)
+	for i := 2; i <= len(cost); i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+	return dp[len(cost)]
+}
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i, _ := range dp {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for i, _ := range dp {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		if obstacleGrid[i][0] == 0 {
+			dp[i][0] = 1
+		} else {
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		if obstacleGrid[0][i] == 0 {
+			dp[0][i] = 1
+		} else {
+			break
+		}
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}

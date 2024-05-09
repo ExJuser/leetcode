5db7f4713f4ab947a154ad9ce00a63c17555510c
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-09 21:37:52 +0800

    不同路径优化

--- Solution3.go
+++ Solution3.go
@@ -512,19 +512,11 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	for i, _ := range dp {
 		dp[i] = make([]int, n)
 	}
-	for i := 0; i < m; i++ {
-		if obstacleGrid[i][0] == 0 {
-			dp[i][0] = 1
-		} else {
-			break
-		}
+	for i := 0; i < m && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
 	}
-	for i := 0; i < n; i++ {
-		if obstacleGrid[0][i] == 0 {
-			dp[0][i] = 1
-		} else {
-			break
-		}
+	for i := 0; i < n && obstacleGrid[0][i] == 0; i++ {
+		dp[0][i] = 1
 	}
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {

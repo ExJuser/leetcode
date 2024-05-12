444956f06c77035a06ac0dbe940dfe9dcd60bf5d
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-12 20:26:42 +0800

    打家劫舍

--- Solution3.go
+++ Solution3.go
@@ -773,3 +773,18 @@ func wordBreak(s string, wordDict []string) bool {
 	}
 	return dp[n-1]
 }
+
+// 打家劫舍
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		//偷或者不偷
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+	return dp[len(nums)-1]
+}

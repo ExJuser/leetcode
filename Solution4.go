35ad3c10418a6f4412fd994893689f3c011f7b02
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-14 22:46:06 +0800

    两个字符串的删除操作

--- Solution4.go
+++ Solution4.go
@@ -151,18 +151,39 @@ func maxSubArray1(nums []int) int {
 
 // 不同的子序列
 // 返回在s的子序列中t出现的个数
+//func numDistinct(s string, t string) int {
+//	dp := make([][]int, len(s)+1)
+//	for i := 0; i < len(dp); i++ {
+//		dp[i] = make([]int, len(t)+1)
+//	}
+//	for i := 0; i < len(dp); i++ {
+//		dp[i][0] = 1
+//	}
+//	for i := 1; i < len(dp); i++ {
+//		for j := 1; j < len(dp[i]); j++ {
+//			if s[i-1] == t[j-1] {
+//				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+//			} else {
+//				dp[i][j] = dp[i-1][j]
+//			}
+//		}
+//	}
+//	return dp[len(s)][len(t)]
+//}
+
 func numDistinct(s string, t string) int {
 	dp := make([][]int, len(s)+1)
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, len(t)+1)
 	}
-	for i := 0; i < len(dp); i++ {
+	//初始化 dp[i][0] dp[0][j]
+	for i := 0; i < len(s); i++ {
 		dp[i][0] = 1
 	}
 	for i := 1; i < len(dp); i++ {
 		for j := 1; j < len(dp[i]); j++ {
 			if s[i-1] == t[j-1] {
-				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
 			} else {
 				dp[i][j] = dp[i-1][j]
 			}
@@ -223,4 +244,33 @@ func findLength(nums1 []int, nums2 []int) (ans int) {
 		}
 	}
 	return
+}
+
+// 两个字符串的删除操作
+// 使得 word1 和 word2 相同所需的最小步数
+func minDistance(word1 string, word2 string) int {
+	//dp[i][j] 代表使得以i-1结尾的word1和j-1结尾的word2相同的最小步数
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+	}
+	//初始化 dp[i][0]
+	//使得一个空串和一个字符串相等的最小步数即字符串长度
+	for i := 0; i < len(dp); i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j < len(dp[0]); j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[i]); j++ {
+			if word1[i-1] == word2[j-1] {
+				//两个字符相同 不需要操作
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+			}
+		}
+	}
+	return dp[len(word1)][len(word2)]
 }

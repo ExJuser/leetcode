af09d3dcc4bab2729c6655791540a84af18d73a1
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-15 21:36:09 +0800

    单调栈

--- Solution4.go
+++ Solution4.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"slices"
+	"strings"
+)
+
 // 最长重复子数组：连续
 //func findLength(nums1 []int, nums2 []int) (ans int) {
 //	dp := make([][]int, len(nums1))
@@ -300,4 +305,167 @@ func minDistance(word1 string, word2 string) int {
 		}
 	}
 	return dp[len(word1)][len(word2)]
+}
+
+func countSubstrings(s string) int {
+	count := 0
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, len(s))
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i; j < len(s); j++ {
+			if i == j {
+				dp[i][j] = true
+				count++
+			} else if j-i == 1 && s[i] == s[j] {
+				dp[i][j] = true
+				count++
+			} else if j-i > 1 && s[i] == s[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// 最长回文子序列
+func longestPalindromeSubseq(s string) int {
+	//dp[i][j] 以i开头j结尾的字符串中最长的回文子序列长度
+	dp := make([][]int, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(s))
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		for j := i; j < len(s); j++ {
+			if i == j {
+				dp[i][j] = 1
+			} else if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][len(s)-1]
+}
+
+// 旋转图像：先按照对角线交换 然后每一行逆序
+func rotate(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix[i]); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < len(matrix); i++ {
+		slices.Reverse(matrix[i])
+	}
+}
+
+// 非原地算法解决
+func gameOfLife(board [][]int) {
+	newBoard := make([][]int, len(board))
+	for i := 0; i < len(newBoard); i++ {
+		newBoard[i] = make([]int, len(board[i]))
+	}
+	copy(newBoard, board)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			count := 0
+			for m := i - 1; m >= 0 && m <= i+1 && m < len(board); m++ {
+				for n := j - 1; n >= 0 && n <= j+1 && n < len(board[i]); n++ {
+					if m != i && n != j && board[m][n] == 1 {
+						count++
+					}
+				}
+			}
+			//活细胞少于两个  活细胞死亡、死细胞仍死亡
+			//活细胞2个 	   活细胞存活、死细胞仍死亡
+			//活细胞3个      活细胞存活、死细胞复活
+			//活细胞超过三个  活细胞死亡 死细胞复活
+			//即活细胞死亡的情况为：活细胞少于两个、活细胞超过三个
+			//死细胞复活的情况为：大于等于三个
+			if board[i][j] == 1 && (count < 2 || count > 3) {
+				newBoard[i][j] = 0
+			}
+			if board[i][j] == 0 && count >= 3 {
+				newBoard[i][j] = 1
+			}
+		}
+	}
+}
+func isValid1(s string) bool {
+	stack := make([]byte, 0)
+	for _, ch := range s {
+		if strings.Contains("([{", string(ch)) {
+			stack = append(stack, byte(ch))
+		} else {
+			if len(stack) == 0 || ch == ')' && stack[len(stack)-1] != '(' ||
+				(ch == ']' && stack[len(stack)-1] != '[') ||
+				(ch == '}' && stack[len(stack)-1] != '{') {
+				return false
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+// 二叉树的中序遍历
+func inorderTraversal1(root *TreeNode) (ans []int) {
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		ans = append(ans, node.Val)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return
+}
+
+func removeKdigits(num string, k int) string {
+	stack := make([]byte, 0)
+	for i := 0; i < len(num); i++ {
+		for len(stack) > 0 && stack[len(stack)-1]-'0' > num[i]-'0' {
+			if k == 0 {
+				break
+			}
+			k--
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, num[i])
+	}
+	for k > 0 {
+		stack = stack[:len(stack)-1]
+		k--
+	}
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		return "0"
+	}
+	return ans
+}
+
+// 移除重复字母且使得字典序最小
+func removeDuplicateLetters(s string) string {
+	count := make(map[byte]int)
+	for _, char := range s {
+		count[byte(char)]++
+	}
+	stack := make([]byte, 0)
+	for _, char := range s {
+		if !slices.Contains(stack, byte(char)) {
+			for len(stack) > 0 && stack[len(stack)-1] >= byte(char) && count[stack[len(stack)-1]] >= 1 {
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, byte(char))
+		}
+		count[byte(char)]--
+	}
+	return string(stack)
 }

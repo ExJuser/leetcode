cdf18b7b3834b100a9f158488b374fe5dcda8f8e
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-27 20:07:38 +0800

    单线程 CPU

--- Solution5.go
+++ Solution5.go
@@ -1,6 +1,9 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"slices"
+)
 
 func maxSatisfied(customers []int, grumpy []int, minutes int) int {
 	var ans, cnt int
@@ -174,6 +177,66 @@ func totalCost(costs []int, k int, candidates int) (ans int64) {
 	for ; k > 0; k-- {
 		heapTop := heap.Pop(candidateHeap).([2]int)
 		ans += int64(heapTop[0])
+	}
+	return
+}
+
+type OrderHeap [][]int
+
+func (o *OrderHeap) Len() int {
+	return len(*o)
+}
+
+func (o *OrderHeap) Less(i, j int) bool {
+	if (*o)[i][1] == (*o)[j][1] {
+		return (*o)[i][2] < (*o)[j][2]
+	}
+	return (*o)[i][1] < (*o)[j][1]
+}
+
+func (o *OrderHeap) Swap(i, j int) {
+	(*o)[i], (*o)[j] = (*o)[j], (*o)[i]
+}
+
+func (o *OrderHeap) Push(x any) {
+	*o = append(*o, x.([]int))
+}
+
+func (o *OrderHeap) Pop() any {
+	x := (*o)[o.Len()-1]
+	*o = (*o)[:o.Len()-1]
+	return x
+}
+
+// 感觉写麻烦了
+func getOrder(tasks [][]int) (ans []int) {
+	orderHeap := &OrderHeap{}
+	tasksWithIndex := make([][]int, len(tasks))
+	for i := 0; i < len(tasks); i++ {
+		//三个分别是任务id 任务进入队列时间 任务耗时
+		tasksWithIndex[i] = []int{i, tasks[i][0], tasks[i][1]}
+	}
+	slices.SortFunc(tasksWithIndex, func(a, b []int) int {
+		if a[1] == b[1] {
+			return a[2] - b[2]
+		}
+		return a[1] - b[1]
+	})
+	var curTime, finishedTask int
+	taskIndex := 0
+	for finishedTask < len(tasksWithIndex) {
+		for taskIndex < len(tasksWithIndex) && curTime >= tasksWithIndex[taskIndex][1] {
+			heap.Push(orderHeap, []int{tasksWithIndex[taskIndex][1], tasksWithIndex[taskIndex][2], tasksWithIndex[taskIndex][0]})
+			taskIndex++
+		}
+		if orderHeap.Len() == 0 {
+			curTime = tasksWithIndex[taskIndex][1]
+		} else {
+			task := heap.Pop(orderHeap).([]int)
+			ans = append(ans, task[2])
+			curTime += task[1]
+			finishedTask++
+		}
 	}
 	return
 }

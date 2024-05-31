af8c54d53a1e12bc9210f182d6bee6898c41684c
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-05-31 16:23:47 +0800

    0531

--- Solution6.go
+++ Solution6.go
@@ -19,6 +19,7 @@ func pathSum1(root *TreeNode, targetSum int) (ans [][]int) {
 	dfs(root, 0, []int{})
 	return
 }
+
 func hasPathSum1(root *TreeNode, targetSum int) bool {
 	var dfs func(node *TreeNode, sum int) bool
 	dfs = func(node *TreeNode, sum int) bool {
@@ -35,4 +36,47 @@ func hasPathSum1(root *TreeNode, targetSum int) bool {
 		return false
 	}
 	return dfs(root, 0)
+}
+func kConcatenationMaxSum1(arr []int, k int) int {
+	var maxSubArray func(nums []int) int
+	maxSubArray = func(nums []int) int {
+		ans := nums[0]
+		for i := 1; i < len(nums); i++ {
+			nums[i] = max(nums[i], (nums[i-1]+nums[i])%(1e9+7))
+			ans = max(ans, nums[i])
+		}
+		return max(ans, 0)
+	}
+	newArr := make([]int, 0, 3*len(arr))
+	for ; k > 0; k-- {
+		newArr = append(newArr, arr...)
+	}
+	return maxSubArray(newArr)
+}
+func removeElement1(nums []int, val int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		for i < n && nums[i] == val {
+			nums[i], nums[n-1] = nums[n-1], nums[i]
+			n--
+		}
+	}
+	return n
+}
+func removeDuplicates1(nums []int) int {
+	index, times := 1, 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[index-1] {
+			times++
+			if times <= 2 {
+				nums[index] = nums[i]
+				index++
+			}
+		} else {
+			times = 1
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
 }

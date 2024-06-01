22e28529a242b52d8f760b9fe4021bafdb27018d
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-01 19:49:18 +0800

    0601

--- Solution6.go
+++ Solution6.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/bits"
 	"slices"
 	"strings"
@@ -154,4 +155,132 @@ func distributeCandies(n int, limit int) int {
 		}
 	}
 	return ans
+}
+
+// 二叉树最大宽度
+func widthOfBinaryTree(root *TreeNode) (ans int) {
+	type Pair struct {
+		index int
+		node  *TreeNode
+	}
+	queue := make([]Pair, 0)
+	queue = append(queue, Pair{index: 1, node: root})
+	for len(queue) > 0 {
+		size := len(queue)
+		ans = max(ans, queue[size-1].index-queue[0].index+1)
+		for i := 0; i < size; i++ {
+			temp := queue[0]
+			queue = queue[1:]
+			if temp.node.Left != nil {
+				queue = append(queue, Pair{index: temp.index * 2, node: temp.node.Left})
+			}
+			if temp.node.Right != nil {
+				queue = append(queue, Pair{index: temp.index*2 + 1, node: temp.node.Right})
+			}
+		}
+	}
+	return
+}
+func maxPathSum1(root *TreeNode) int {
+	//每一个节点的最大路径和为左子树+右子树
+	//但是返回给上层节点的是两者中的最大值
+	ans := math.MinInt
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		ans = max(ans, node.Val, node.Val+left, node.Val+right, node.Val+left+right)
+		return max(left, right, 0) + node.Val
+	}
+	dfs(root)
+	return ans
+}
+
+func invertTree1(root *TreeNode) *TreeNode {
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		node.Left, node.Right = node.Right, node.Left
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return root
+}
+
+func isCompleteTree(root *TreeNode) bool {
+	sequence := make([]int, 0, 1000)
+	var dfs func(node *TreeNode, code int)
+	dfs = func(node *TreeNode, code int) {
+		if node == nil {
+			return
+		}
+		sequence = append(sequence, code)
+		dfs(node.Left, code*2)
+		dfs(node.Right, code*2+1)
+	}
+	dfs(root, 1)
+	slices.Sort(sequence)
+	for i := 0; i < len(sequence); i++ {
+		if sequence[i] != i+1 {
+			return false
+		}
+	}
+	return true
+}
+
+// 寻找第k大 小根堆
+//
+//	func findTargetNode(root *TreeNode, cnt int) int {
+//		hp := &IntHeap{}
+//		var dfs func(node *TreeNode)
+//		dfs = func(node *TreeNode) {
+//			if node == nil {
+//				return
+//			}
+//			heap.Push(hp, node.Val)
+//			if hp.Len() > cnt {
+//				heap.Pop(hp)
+//			}
+//			dfs(node.Left)
+//			dfs(node.Right)
+//		}
+//		dfs(root)
+//		return heap.Pop(hp).(int)
+//	}
+func findTargetNode(root *TreeNode, cnt int) int {
+	//在当前子树中寻找第cnt大的数字
+	//如果右子树的节点数量>cnt-1:递归右子树寻找第cnt大的数字
+	//如果右子树的节点数量=cnt-1:返回当前节点
+	//如果右子树的节点数量<cnt-1:递归左子树寻找第cnt-right-1大的数字
+	//数量、答案
+	var dfsNodeCount func(node *TreeNode) int
+	var dfsFindTarget func(node *TreeNode, cnt int) int
+	dfsNodeCount = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		return dfsNodeCount(node.Left) + dfsNodeCount(node.Right) + 1
+	}
+	dfsFindTarget = func(node *TreeNode, cnt int) int {
+		if node == nil {
+			return -1
+		}
+		rightNodeCount := dfsNodeCount(node.Right)
+		ans := 0
+		if rightNodeCount > cnt-1 {
+			ans = dfsFindTarget(node.Right, cnt)
+		} else if rightNodeCount == cnt-1 {
+			ans = node.Val
+		} else {
+			ans = dfsFindTarget(node.Left, cnt-rightNodeCount-1)
+		}
+		return ans
+	}
+	return dfsFindTarget(root, cnt)
 }

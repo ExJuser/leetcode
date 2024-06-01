1dd2c37e6db808d1b332ffb8ff647190e3035f05
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-06-01 15:55:15 +0800

    0601

--- Solution6.go
+++ Solution6.go
@@ -1,6 +1,10 @@
 package main
 
-import "math/bits"
+import (
+	"math/bits"
+	"slices"
+	"strings"
+)
 
 func pathSum1(root *TreeNode, targetSum int) (ans [][]int) {
 	var dfs func(node *TreeNode, sum int, path []int)
@@ -99,4 +103,55 @@ func hammingWeight(n int) int {
 }
 func reverseBits(num uint32) uint32 {
 	return bits.Reverse32(num)
+}
+func asteroidCollision(asteroids []int) []int {
+	stack := make([]int, 0, len(asteroids))
+	for i := 0; i < len(asteroids); i++ {
+		crash := false
+		for len(stack) > 0 && stack[len(stack)-1] > 0 && asteroids[i] < 0 {
+			if Abs(stack[len(stack)-1]) < Abs(asteroids[i]) {
+				stack = stack[:len(stack)-1]
+			} else {
+				if stack[len(stack)-1]+asteroids[i] == 0 {
+					stack = stack[:len(stack)-1]
+				}
+				crash = true
+				break
+			}
+		}
+		if !crash {
+			stack = append(stack, asteroids[i])
+		}
+	}
+	return stack
+}
+func reverseVowels(s string) string {
+	bytes := make([]byte, 0, len(s))
+	for i := 0; i < len(s); i++ {
+		if strings.Contains("aoieu", strings.ToLower(s[i:i+1])) {
+			bytes = append(bytes, s[i])
+		}
+	}
+	slices.Reverse(bytes)
+	newS := []byte(s)
+	index := 0
+	for i := 0; i < len(newS); i++ {
+		if strings.Contains("aoieu", strings.ToLower(string(newS[i]))) {
+			newS[i] = bytes[index]
+			index++
+		}
+	}
+	return string(newS)
+}
+func distributeCandies(n int, limit int) int {
+	ans := 0
+	for i := 0; i <= limit; i++ {
+		for j := 0; j <= limit; j++ {
+			k := n - i - j
+			if k >= 0 && k <= limit {
+				ans++
+			}
+		}
+	}
+	return ans
 }

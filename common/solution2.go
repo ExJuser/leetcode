a42222617056eba8713f206978ff61b06ec43434
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-07-30 17:04:51 +0800

    0730

--- common/solution2.go
+++ common/solution2.go
@@ -937,26 +937,68 @@ func maxSlidingWindow(nums []int, k int) (ans []int) {
 	return
 }
 
-// 82. 删除排序链表中的重复元素 II
+//  82. 删除排序链表中的重复元素 II 迭代法
+//     func deleteDuplicates(head *ListNode) *ListNode {
+//     dummy := &ListNode{Next: head}
+//     slow, fast := dummy, head
+//     for fast != nil && fast.Next != nil {
+//     if fast.Val == fast.Next.Val {
+//     dup := fast.Val
+//     for fast != nil && fast.Val == dup {
+//     fast = fast.Next
+//     }
+//     } else {
+//     slow.Next.Val = fast.Val
+//     slow = slow.Next
+//     fast = fast.Next
+//     }
+//     }
+//     if fast != nil {
+//     slow.Next.Val = fast.Val
+//     slow = slow.Next
+//     }
+//     slow.Next = nil
+//     return dummy.Next
+//     }
+//
+// 82. 删除排序链表中的重复元素 II 递归法
+//func deleteDuplicates(head *ListNode) *ListNode {
+//	var dfs func(node *ListNode) *ListNode
+//	dfs = func(node *ListNode) *ListNode {
+//		if node == nil || node.Next == nil {
+//			return node
+//		}
+//		if node.Val != node.Next.Val {
+//			node.Next = dfs(node.Next)
+//			return node
+//		} else {
+//			val := node.Val
+//			for node != nil && node.Val == val {
+//				node = node.Next
+//			}
+//			return dfs(node)
+//		}
+//	}
+//	return dfs(head)
+//}
+
+// 83. 删除排序链表中的重复元素 保留一个
 func deleteDuplicates(head *ListNode) *ListNode {
-	dummy := &ListNode{Next: head}
-	slow, fast := dummy, head
-	for fast != nil && fast.Next != nil {
-		if fast.Val == fast.Next.Val {
-			dup := fast.Val
-			for fast != nil && fast.Val == dup {
-				fast = fast.Next
-			}
+	var dfs func(node *ListNode) *ListNode
+	dfs = func(node *ListNode) *ListNode {
+		if node == nil || node.Next == nil {
+			return node
+		}
+		if node.Val != node.Next.Val {
+			node.Next = dfs(node.Next)
+			return node
 		} else {
-			slow.Next.Val = fast.Val
-			slow = slow.Next
-			fast = fast.Next
+			val := node.Val
+			for node != nil && node.Val == val {
+				node = node.Next
+			}
+			return &ListNode{Val: val, Next: dfs(node)}
 		}
 	}
-	if fast != nil {
-		slow.Next.Val = fast.Val
-		slow = slow.Next
-	}
-	slow.Next = nil
-	return dummy.Next
+	return dfs(head)
 }

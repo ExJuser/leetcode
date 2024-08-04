3daa7447f196a18fc7ee2869f252d2e8b5f6d296
Chenchen Zhu <zhuchenchen1001@qq.com>
2024-08-04 19:41:46 +0800

    1162.地图分析

--- common/solution3.go
+++ common/solution3.go
@@ -822,3 +822,49 @@ func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	}
 	return res
 }
+
+func maxDistance(grid [][]int) int {
+	queue := make([][2]int, 0, len(grid)*len(grid[0]))
+	visited := make([][]bool, len(grid))
+	for i := 0; i < len(visited); i++ {
+		visited[i] = make([]bool, len(grid[i]))
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 1 {
+				queue = append(queue, [2]int{i, j})
+				visited[i][j] = true
+			}
+		}
+	}
+	if len(queue) == 0 || len(queue) == cap(queue) {
+		return -1
+	}
+	dist := 0
+	for len(queue) > 0 {
+		size := len(queue)
+		dist++
+		for i := 0; i < size; i++ {
+			head := queue[0]
+			queue = queue[1:]
+			x, y := head[0], head[1]
+			if x-1 >= 0 && !visited[x-1][y] {
+				queue = append(queue, [2]int{x - 1, y})
+				visited[x-1][y] = true
+			}
+			if y-1 >= 0 && !visited[x][y-1] {
+				queue = append(queue, [2]int{x, y - 1})
+				visited[x][y-1] = true
+			}
+			if x+1 < len(grid) && !visited[x+1][y] {
+				queue = append(queue, [2]int{x + 1, y})
+				visited[x+1][y] = true
+			}
+			if y+1 < len(grid[0]) && !visited[x][y+1] {
+				queue = append(queue, [2]int{x, y + 1})
+				visited[x][y+1] = true
+			}
+		}
+	}
+	return dist
+}
